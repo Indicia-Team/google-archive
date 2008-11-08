a784ca8ca340d8c88560676cfdd156b405a3df36
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-08 09:22:43 +0000

    Working gridview.
    Remains to be done:
    
    -Accurate count

--- trunk/application/controllers/termlist.php
+++ trunk/application/controllers/termlist.php
@@ -8,12 +8,20 @@ class Termlist_Controller extends Opal_Controller {
 		url::redirect('termlist/page/1/5');
 	}
 	public function page($page_no,$limit) {
-		// Generate a new termlist object
 		$model = ORM::factory('termlist');
-		$this->template->title = "Pagination";
-		$this->template->message = 'Termlists grid'; 
-		$termlist = new View('termlist');
-		$termlist->termtable = Gridview_Controller::factory($model,$page_no,$limit)->display();
-		$this->template->content = $termlist;
+		if (!request::is_ajax()) {
+			// Generate a new termlist object
+			$this->template->title = "Pagination";
+			$this->template->message = 'Termlists grid'; 
+			$termlist = new View('termlist');
+			$termlist->termtable = 
+				Gridview_Controller::factory($model,$page_no,$limit)->display();
+			$this->template->content = $termlist;
+		} else {
+			$this->auto_render = false;
+			return $termlist->termtable = 
+				Gridview_Controller::factory($model,$page_no,$limit)->display();
+		}
+
 	}
 }
