6d2b030743ca1f69fd74c853aba9ed1b6af1e2dd
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2008-11-11 16:39:23 +0000

    Various work on websites, saving new websites.

--- trunk/application/views/termlist.php
+++ trunk/application/views/termlist.php
@@ -1,7 +1,7 @@
 <div class="termlist">
 <?php echo $termtable ?>
 <br/>
-<form action="/index.php/termlist/create">
+<form action=action="<?php echo url::site(); ?>termlist/create">>
 <input type="submit" value="New termlist" />
 </form>
 </div>
