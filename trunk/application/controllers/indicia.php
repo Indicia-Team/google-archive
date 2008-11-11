16253b23d7f704201f4597817843f38f8ace941a
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-11 10:54:53 +0000

    Added basic menu entries for the template

--- trunk/application/controllers/indicia.php
+++ trunk/application/controllers/indicia.php
@@ -4,5 +4,11 @@ class Indicia_Controller extends Template_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->db = Database::instance();
+
+		$this->template->links = array
+		(
+			'Home' => 'home',
+			'Term Lists' => 'termlist',
+		);
 	}
 }
