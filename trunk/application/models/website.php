1f8752e248c05e1136946749ade080acb9b6371b
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-12 13:17:16 +0000

    Removed standard chars validation as doesn't allow apostrophes.

--- trunk/application/models/website.php
+++ trunk/application/models/website.php
@@ -7,8 +7,7 @@ class Website_Model extends ORM {
 	public function validate(Validation $array, $save = FALSE) {
 		// uses PHP trim() to remove whitespace from beginning and end of all fields before validation
 		$array->pre_filter('trim');
-		$array->add_rules('title', 'required', 'length[1,100]', 'standard_text');
-		$array->add_rules('description', 'standard_text');
+		$array->add_rules('title', 'required', 'length[1,100]');
 		return parent::validate($array, $save);
 	}
 }
