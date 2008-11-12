4d46442b78ceebf0d42fe92b060fae22731d4dce
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2008-11-12 08:41:49 +0000

    Pass model to view rather than individual fields.

--- trunk/application/views/website.php
+++ trunk/application/views/website.php
@@ -1,5 +1,6 @@
 <div class="termlist">
 <?php echo $table ?>
+<br />
 <form action="<?php echo url::site(); ?>website/create">
 <input type="submit" value="New website" />
 </form>
--- trunk/application/views/website_edit.php
+++ trunk/application/views/website_edit.php
@@ -1,16 +1,16 @@
-<p>This page allows you to specify a new website that will use the services provided by this Indicia Core Module instance.</p>
+<p>This page allows you to specify the details of a website that will use the services provided by this Indicia Core Module instance.</p>
 <form class="cmxform" action="<?php echo url::site().'website/save'; ?>" method="post">
-<input type="hidden" name="id" id="id" value="<?php echo html::specialchars($form['id']) ?>" />
+<input type="hidden" name="id" id="id" value="<?php echo html::specialchars($website->id); ?>" />
 <fieldset>
 <legend>Website details</legend>
 <ol>
 <li>
 <label for="title">Title</label>
-<input id="title" name="title" value="<?php echo html::specialchars($form['title']) ?>" />
+<input id="title" name="title" value="<?php echo html::specialchars($website->title); ?>" />
 </li>
 <li>
 <label for="description">Description</label>
-<textarea rows="7" id="description" name="description"><?php echo html::specialchars($form['description']); ?></textarea>
+<textarea rows="7" id="description" name="description"><?php echo html::specialchars($website->description); ?></textarea>
 </li>
 </ol>
 </fieldset>
