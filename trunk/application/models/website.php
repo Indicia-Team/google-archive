71f4c9f755b951898682acbb375a80677bc3fd69
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-12 14:23:16 +0000

    Fixed an error

--- trunk/application/models/website.php
+++ trunk/application/models/website.php
@@ -7,11 +7,8 @@ class Website_Model extends ORM {
 	public function validate(Validation $array, $save = FALSE) {
 		// uses PHP trim() to remove whitespace from beginning and end of all fields before validation
 		$array->pre_filter('trim');
-<<<<<<< .mine
 		$array->add_rules('title', 'required', 'length[1,100]');
-=======
 		$array->add_rules('title', 'required', 'length[1,100]');
->>>>>>> .r52
 		return parent::validate($array, $save);
 	}
 }
