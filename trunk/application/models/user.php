c6cddfc038f56daf5834a417f7d6694ae4448e2c
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2008-11-13 08:47:50 +0000

    Model linkages to User

--- trunk/application/models/user.php
+++ trunk/application/models/user.php
@@ -3,6 +3,10 @@
 class User_Model extends ORM {
 
 	protected $belongs_to = array('person');
-	protected $has_many = array('termlist'=>'created_by');
+	protected $has_many = array(
+		'termlist'=>'created_by','termlist'=>'updated_by',
+		'website'=>'created_by','website'=>'updated_by',
+		'location'=>'created_by','location'=>'updated_by',
+		);
 
 }
--- trunk/application/models/website.php
+++ trunk/application/models/website.php
@@ -3,6 +3,8 @@
 class Website_Model extends ORM {
 
 	protected $has_many = array('termlist');
+	protected $belongs_to = array('created_by'=>'user', 'updated_by'=>'user');
+	protected $has_and_belongs_to_many = array('locations');
 
 	public function validate(Validation $array, $save = FALSE) {
 		// uses PHP trim() to remove whitespace from beginning and end of all fields before validation
