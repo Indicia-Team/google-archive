2f78137cd1ebad33e28082e83dd749559037c917
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2008-11-13 08:40:31 +0000

    Updated to use new Gridview base controller class.

--- trunk/application/controllers/website.php
+++ trunk/application/controllers/website.php
@@ -1,27 +1,13 @@
 <?php
 
-class Website_Controller extends Indicia_Controller {
-	public function __construct() {
-		parent::__construct();
-	}
-	public function page($page_no,$limit) {
-		$model = ORM::factory('website');
-		// Generate a new termlist object
-		$this->template->title = "Websites";
-		$this->template->message = 'Websites grid';
-		$website = new View('website');
-		$grid = Gridview_Controller::factory($model,$page_no,$limit,3,null);
-		// Hide the first (id) column
-		array_splice($grid->columns,0,1);
-        $website->table = $grid->display();
-		$this->template->content = $website;
-	}
+class Website_Controller extends Gridview_Base_Controller {
 
-	// Auxilliary function for handling Ajax requests from the page method gridview component
-	public function page_gv($page_no,$limit) {
-		$model = ORM::factory('website');
-		$this->auto_render = false;
-		return Gridview_Controller::factory($model,$page_no,$limit,3,null)->display();
+	public function __construct() {
+		parent::__construct(ORM::factory('website'), new View('website'));
+		$this->columns = array(
+			'title'=>'',
+			'description'=>'');
+		$this->pagetitle = "Websites";
 	}
 
 	public function create() {
