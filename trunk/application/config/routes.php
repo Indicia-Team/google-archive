711218c4d3f7e7612e8443846db2b1d87bb7a47c
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-13 11:59:06 +0000

    Emended taxon_list_edit to routes.

--- trunk/application/config/routes.php
+++ trunk/application/config/routes.php
@@ -7,6 +7,7 @@
 $config['_default'] = 'welcome';
 $config['termlist'] = 'termlist/page/1/5';
 $config['termlist/edit/([0-9]+)'] = 'termlist/edit/$1/1/5';
+$config['taxon_list/edit/([0-9]+)'] = 'taxon_list/edit/$1/1/5';
 $config['website'] = 'website/page/1/5';
 $config['taxon_group'] = 'taxon_group/page/1/5';
 $config['taxon_list'] = 'taxon_list/page/1/5';
