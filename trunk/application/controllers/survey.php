bb4730b5a77506241882b80cd351e135830a219d
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2008-11-14 10:38:32 +0000

    Basic work on surveys - not yet complete.

---
+++ trunk/application/controllers/survey.php
@@ -0,0 +1,54 @@
+<?php
+
+class Survey_Controller extends Gridview_Base_Controller {
+
+	public function __construct() {
+		parent::__construct(ORM::factory('gv_survey'), new View('survey/survey'));
+		$this->columns = array(
+			'title'=>'',
+			'description'=>'',
+			'website'=>'');
+		$this->pagetitle = "Surveys";
+	}
+
+	public function create() {
+		$this->template->title = "Create New Survey";
+		$view = new View('survey/survey_edit');
+		// Create a new survey model to pass to the view
+		$view->model = ORM::factory('survey');
+		$this->template->content = $view;
+	}
+
+	public function edit() {
+		if ($this->uri->total_arguments()==0)
+			print "cannot edit survey without an ID";
+		else
+		{
+			$survey = new Survey_Model($this->uri->argument(1));
+			$this->template->title = "Edit ".$survey->title;
+			$view = new View('survey/survey_edit');
+			$view->model = $survey;
+			$this->template->content = $view;
+		}
+	}
+
+	public function save() {
+		if (! empty($_POST['id']))
+			$survey = new Survey_Model($_POST['id']);
+		else
+			$survey = new Survey_Model();
+		$_POST = new Validation($_POST);
+		if ($survey->validate($_POST, TRUE)) {
+			url::redirect('survey');
+		} else {
+			// errors are now embedded in the model
+		    $this->template->title = "Edit ".$survey->title;
+			$view = new View('survey/survey_edit');
+			$view->model = $survey;
+			$this->template->content = $view;
+		}
+
+	}
+}
+
+?>
---
+++ trunk/application/views/survey/survey.php
@@ -0,0 +1,7 @@
+<div class="survey">
+<?php echo $table ?>
+<br/>
+<form action="<?php echo url::site().'survey/create'; ?>" method="post">
+<input type="submit" value="New survey" />
+</form>
+</div>
