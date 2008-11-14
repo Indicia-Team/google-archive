3bb9563d2dd9710fb134e337150db70103242d8f
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-14 15:23:15 +0000

    Corrected legend

--- trunk/application/views/taxon_group_edit.php
+++ trunk/application/views/taxon_group_edit.php
@@ -2,7 +2,7 @@
 <form class="cmxform" action="<?php echo url::site().'taxon_group/save'; ?>" method="post">
 <input type="hidden" name="id" id="id" value="<?php echo html::specialchars($taxon_group->id); ?>" />
 <fieldset>
-<legend>taxon_group details</legend>
+<legend>Taxon Group details</legend>
 <ol>
 <li>
 <label for="title">Title</label>
