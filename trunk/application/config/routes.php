d21fa43e873361a8c94f10cdf351f1b00ed7c194
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-14 12:29:04 +0000

    Added language CRUD functionality

--- trunk/application/config/routes.php
+++ trunk/application/config/routes.php
@@ -5,10 +5,20 @@
  * Sets the default route to "welcome"
  */
 $config['_default'] = 'welcome';
+
+// Termlist
 $config['termlist'] = 'termlist/page/1/5';
 $config['termlist/edit/([0-9]+)'] = 'termlist/edit/$1/1/5';
-$config['taxon_list/edit/([0-9]+)'] = 'taxon_list/edit/$1/1/5';
-$config['website'] = 'website/page/1/5';
-$config['survey'] = 'survey/page/1/5';
-$config['taxon_group'] = 'taxon_group/page/1/5';
+
+// Taxon list
 $config['taxon_list'] = 'taxon_list/page/1/5';
+$config['taxon_list/edit/([0-9]+)'] = 'taxon_list/edit/$1/1/5';
+
+// Website
+$config['website'] = 'website/page/1/5';
+// Survey
+$config['survey'] = 'survey/page/1/5';
+// Taxon Group
+$config['taxon_group'] = 'taxon_group/page/1/5';
+// Language
+$config['language'] = 'language/page/1/5';
