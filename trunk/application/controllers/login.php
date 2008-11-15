11f60792c1fc980a14c9588702fc6f47f87a1007
vanbreda <vanbreda@btinternet.com>
2008-11-15 23:15:55 +0000

    Added functionality to check CORE_ROLES during authentication - the presence of any CORE_ROLE for a user will allow logon; no CORE_ROLE, no logon. In additional sorted logon by email address.

--- trunk/application/controllers/login.php
+++ trunk/application/controllers/login.php
@@ -4,12 +4,10 @@ class Login_Controller extends Indicia_Controller {
 
 // As it stands, login does not check passwords, roles, and remeber me doesn't work.
 // Things to do (in order):
-// 1 Enable role checking
-// 2 Enable Login by email
-// 3 Create Lost Password functionality
-// 4 Enable password checking
-// 5 Enable remember me functionality
-// 6 Put in hooks to auto redirect to login if not logged in
+// 1 Create Lost Password functionality
+// 2 Enable password checking
+// 3 Enable remember me functionality
+// 4 Put in hooks to auto redirect to login if not logged in
 	
 	public function index()
 	{
@@ -23,22 +21,30 @@ class Login_Controller extends Indicia_Controller {
 		{
 			$this->template->title = 'Already Logged In';
 			$this->template->content = new View('login_message');	
-			$this->template->content->message = 'You are already logged in.';
+			$this->template->content->message = 'You are already logged in.<br />';
 			$this->template->content->link_to_home = 'YES';
 			$this->template->content->link_to_logout = 'YES';
 			return;
 		}
 		$this->template->title = 'User Login';
 		$this->template->content = new View('login_by_username');	
-		$this->template->content->error_message = 'At this time validation only occurs on username: no checks done on Password or Roles. Remember me does not work, nor does forgotten password.';
+		$this->template->content->error_message = 'At this time validation only occurs on username and core_role: no checks done on Password. Remember me does not work, nor does forgotten password.';
 		if (request::method() == 'post')
 		{
 			if ($this->auth->login(array('username' => $_POST['UserName']), $_POST['Password']))
 			{
-				url::redirect();
-				return;
+// I don't trust the results!! There is something funny going on where the
+// number of rows in a query is not being reported correctly - an invalid username returns
+// a valid login with the first real user.
+// THIS IS A DOUBLE CHECK. IF THE USERNAME DOESN'T MATCH, FORCE A LOG OFF.
+				if ($_POST['UserName'] == $_SESSION['auth_user']->username)
+				{
+					url::redirect();
+					return;
+				}
+				$this->auth->logout(TRUE);
 			}
-			$this->template->content->error_message = 'Invalid Username/Password Combination, or insufficient privileges';
+			$this->template->content->error_message = 'Invalid Email address/Password Combination, or insufficient privileges';
 		}
 	}
 
@@ -56,7 +62,7 @@ class Login_Controller extends Indicia_Controller {
 		}
 		$this->template->title = 'User Login';
 		$this->template->content = new View('login_by_email');	
-		$this->template->content->error_message = 'At this time this is not implemented';
+		$this->template->content->error_message = 'At this time validation only occurs on username and core_role: no checks done on Password. Remember me does not work, nor does forgotten password.';
 		if ( $login_config['login_by_email'] != 'YES')
 		{
 			$this->template->content->link_to_username = 'YES';
@@ -66,7 +72,9 @@ class Login_Controller extends Indicia_Controller {
 		{
 			# this is name complete as needs to convert from email address to username
 			# or to extend auth model
-			if ($this->auth->login($_POST['UserName'], $_POST['Password']))
+			$person = ORM::factory('person', array('email_address' => $_POST['Email']));
+			
+			if ($this->auth->login(array('person_id' => $person->id), $_POST['Password']))
 			{
 				url::redirect();
 				return;
--- trunk/modules/indicia_auth/models/user.php
+++ trunk/modules/indicia_auth/models/user.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class User_Model extends Auth_User_Model {
-	
-	// This class can be replaced or extended
-	
+
+	protected $belongs_to = array('core_role');
+		
 } // End User Model
