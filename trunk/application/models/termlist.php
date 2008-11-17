b4453f6ca4c6cc477f02cbfc8fa8e38058b3307a
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-17 12:36:00 +0000

    Termlists_term model

--- trunk/application/models/termlist.php
+++ trunk/application/models/termlist.php
@@ -1,10 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+
 class Termlist_Model extends ORM_Tree {
 
 	protected $children = "termlists";
 	protected $belongs_to = array('website', 'created_by'=>'user', 'updated_by'=>'user');
 	protected $has_and_belongs_to_many = array('terms');
+	protected $has_many = array('termlists_terms');
 
 	public function validate(Validation $array, $save = FALSE) {
 		$array->pre_filter('trim');
