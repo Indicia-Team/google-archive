20a358f5871d11c8a7130ab567587feb51bb614c
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2008-11-19 12:44:29 +0000

    Fixed nulls in grid.

--- trunk/application/views/gridview_body.php
+++ trunk/application/views/gridview_body.php
@@ -1,12 +1,14 @@
-<?php 
+<?php
 $i = 0;
 foreach ($table as $item) {
 	echo "<tr class='";
 	echo ($i%2 == 0) ? "evenRow" : "oddRow";
 	echo "'>";
 	foreach (array_intersect_key($item->as_array(), $columns) as $field) {
-		echo "<td>".html::anchor($this->uri->segment(1)
-			."/edit/".$item->id, $field)."</td>";
+		echo "<td>";
+		if ($field!==NULL)
+			echo html::anchor($this->uri->segment(1)."/edit/".$item->id, $field);
+		echo "</td>";
 	}
 	$i++;
 	echo "</tr>";
