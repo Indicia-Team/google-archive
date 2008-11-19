ad4319eacbdfac6513be900d6fcca1e38db8c3f9
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2008-11-19 09:16:49 +0000

    Retested db build, tidied up so scripts do run in sequence.

--- trunk/application/db/200811131748_create_gv_termlist.sql
+++ trunk/application/db/200811131748_create_gv_termlist.sql
@@ -1,6 +1,6 @@
 -- View: gv_termlists
 
-DROP VIEW gv_termlists;
+-- DROP VIEW gv_termlists;
 
 CREATE OR REPLACE VIEW gv_termlists AS 
  SELECT t.id, t.title, t.description, t.website_id, t.parent_id, t.deleted, t.created_on, t.created_by_id, t.updated_on, t.updated_by_id, w.title AS website, p.surname AS creator
@@ -8,5 +8,3 @@ CREATE OR REPLACE VIEW gv_termlists AS
    LEFT JOIN websites w ON t.website_id = w.id
    INNER JOIN users u ON t.created_by_id = u.id
    INNER JOIN people p ON u.person_id = p.id;
-
-ALTER TABLE gv_termlists OWNER TO opal;
