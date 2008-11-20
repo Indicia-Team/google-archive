df665d83175b9b1f959f50b0acd14ef06ce91a43
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-20 09:18:18 +0000

    Fixed an error in create script for termlists_terms

--- trunk/application/db/200811171400_gv_termlists_terms.sql
+++ trunk/application/db/200811171400_gv_termlists_terms.sql
@@ -1,8 +1,8 @@
 -- View: gv_termlist_terms
 
--- DROP VIEW gv_termlist_terms;
+-- DROP VIEW gv_termlists_terms;
 
-CREATE OR REPLACE VIEW gv_termlist_terms AS 
+CREATE OR REPLACE VIEW gv_termlists_terms AS 
  SELECT tt.id, tt.termlist_id, tt.term_id, tt.created_on, tt.created_by_id, tt.updated_on, tt.updated_by_id, tt.parent_id, tt.meaning_id, tt.preferred, tt.sort_order, t.term, l.language
    FROM termlists_terms tt
    JOIN terms t ON tt.term_id = t.id
