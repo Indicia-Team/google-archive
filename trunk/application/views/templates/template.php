2a7bdc8c1453c43ab42203487ba2d950776b97e1
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-22 19:02:12 +0000

    Template provided by Armand Tupel

--- trunk/application/views/templates/template.php
+++ trunk/application/views/templates/template.php
@@ -2,59 +2,90 @@
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+
+<!-- Main template -->
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <meta id="baseURL" name="baseURI" content="<?php echo url::site() ?>" />
 <meta id="routedURI" name="routedURI" content="<?php echo url::site().router::$routed_uri; ?>" />
+
+<?php echo html::stylesheet(array('media/css/site',),array('screen',)); ?>
+<?php echo html::stylesheet(array('media/css/forms',),array('screen',)); ?>
+
+<!-- BEGIN: jquery/superfish init -->
 <?php
-	echo html::script(array(
-		    'media/js/jquery-1.2.6.js',
-		    'media/js/jquery.url.js',
-		    'media/js/hasharray.js',
-			'media/js/superfish',
-		), FALSE);
-	echo html::stylesheet(array('media/css/site',),array('screen',));
-	echo html::stylesheet(array('media/css/menus',),array('screen',));
-	echo html::stylesheet(array('media/css/forms',),array('screen',));
+    echo html::script(array(
+            'media/js/jquery-1.2.6.js',
+            'media/js/jquery.url.js',
+            'media/js/hasharray.js',
+            'media/js/superfish.js',
+        ), FALSE);
 ?>
+
+<?php echo html::stylesheet(array('media/css/menus',),array('screen',)); ?>
+
 <script type="text/javascript">
-	  $(document).ready(function() {
+      $(document).ready(function() {
         $('ul.sf-menu').superfish();
     });
 </script>
+<!-- END: jquery/superfish init -->
+
 <title><?php echo html::specialchars($title) ?></title>
+
 </head>
 <body>
-<div id="wrapper">
-<div id="banner">
-<span>Indicia</span>
-</div>
-<ul class="sf-menu">
-<?php
-	$temp=array_keys($menu);
-	$lastitem = $temp[count($menu)-1];
-	foreach ($menu as $toplevel => $submenu):
-		if (count($submenu)==0)
-			// No submenu, so treat as link to the home page
-			$toplevel = html::anchor('home', $toplevel);
-		if ($toplevel==$lastitem)
-			echo '<li class="last">'.$toplevel;
-		else
-			echo '<li>'.$toplevel;
 
-		if (count($submenu)>0)
-		{
-			echo '<ul>';
-			foreach ($submenu as $menuitem => $url):
-				echo '<li>'.html::anchor($url, $menuitem).'</li>';
-			endforeach;
-			echo '</ul>';
-		}
-		echo '</li>';
-	endforeach;
-	 ?>
-</ul>
-<div id="content">
-<h1><?php echo $title ?></h1>
-<?php echo $content ?>
+<div id="wrapper">
+
+    <!-- BEGIN: banner -->
+    <div id="banner" role="banner">
+        <span>Indicia</span>
+    </div>
+    <!-- END: banner -->
+
+    <!-- BEGIN: main menu (jquery/superfish) -->
+    <ul class="sf-menu" role="menubar">
+
+    <?php foreach ($menu as $toplevel => $submenu): ?>
+
+        <!-- BEGIN: print the top level menu items -->
+        <?php if(count($submenu)==0): ?>
+            <!-- No submenu, so treat as link to the home page -->
+            <li role="menuitem"><?php echo html::anchor('home', $toplevel); ?>
+        <?php else: ?>
+            <li role="menu"><a href="#"><?php echo $toplevel; ?></a>
+        <?php endif; ?>
+
+            <!-- BEGIN: print the sub menu items -->
+            <?php if (count($submenu)>0): ?>
+                <ul>
+                <?php foreach ($submenu as $menuitem => $url): ?>
+                    <li role="menuitem"><?php echo html::anchor($url, $menuitem); ?></li>
+                <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+            <!-- END: print the sub menu items -->
+
+        </li>
+        <!-- END: print the top level menu items -->
+
+    <?php endforeach; ?>
+
+    </ul>
+    <!-- END: main menu (jquery/superfish) -->
+
+    <!-- BEGIN: page level content -->
+    <div id="content" role="main">
+
+        <h1><?php echo $title; ?></h1>
+
+        <?php echo $content; ?>
+
+    </div>
+    <!-- END: page level content -->
+
+</div>
+
 </body>
 </html>
