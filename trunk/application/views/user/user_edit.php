741eb55f67e424b87f634b696f68515d2531b43e
vanbreda@btinternet.com <vanbreda@btinternet.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-24 10:44:58 +0000

    Adds Core Role field to Users form.

--- trunk/application/views/user/user_edit.php
+++ trunk/application/views/user/user_edit.php
@@ -32,6 +32,22 @@
 <label for="view_common_names">Show Common Names</label>
 <?=form::checkbox('view_common_names', TRUE, isset($model->view_common_names) AND ($model->view_common_names == 't') ) ?>
 </li>
+<li>
+<label for="core_role_id">Role within CORE Module</label>
+<select id="core_role_id" name="core_role_id">
+	<option>None</option>
+<?php
+	$core_roles = ORM::factory('core_role')->orderby('title','asc')->find_all();
+	foreach ($core_roles as $core_role) {
+		echo '	<option value="'.$core_role->id.'" ';
+		if ($core_role->id==$model->core_role_id)
+			echo 'selected="selected" ';
+		echo '>'.$core_role->title.'</option>';
+	}
+?>
+</select>
+<?php echo html::error_message($model->getError('core_role_id')); ?>
+</li>
 </ol>
 </fieldset>
 <?php echo $metadata ?>
