9f2c8689d49d7b8eb21ee26276725af299625f61
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-25 16:51:00 +0000

    Changed default number of items to show in gridview, added Armand's website controller.

--- trunk/application/controllers/website.php
+++ trunk/application/controllers/website.php
@@ -1,60 +1,117 @@
 <?php
+/**
+ * INDICIA
+ * @link http://code.google.com/p/indicia/
+ * @package Indicia
+ */
 
-class Website_Controller extends Gridview_Base_Controller {
+/**
+ * Website page controller
+ *
+ *
+ * @package Indicia
+ * @subpackage Controller
+ * @license http://www.gnu.org/licenses/gpl.html GPL
+ * @author xxxxxxx <xxx@xxx.net> / $Author$
+ * @copyright xxxx
+ * @version $Rev$ / $LastChangedDate$
+ */
+class Website_Controller extends Gridview_Base_Controller
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct('website', 'website', 'website/index');
 
-	public function __construct() {
-		parent::__construct('website', 'website', 'website/index');
-		$this->columns = array(
-			'title'=>'',
-			'description'=>'');
-		$this->pagetitle = "Websites";
-	}
+        $this->columns = array('title'       =>'',
+                               'description' =>'' );
 
-	/**
-	 * Action for website/create page.
-	 * Displays a page allowing entry of a new website.
-	 */
-	public function create() {
-		$model = ORM::factory('website');
-		$view = new View('website/website_edit');
-		$view->model = $model;
-		$view->metadata = $this->GetMetadataView($model);
-		$this->template->title = $this->GetEditPageTitle($model, 'Website');
-		$this->template->content = $view;
-	}
+        $this->pagetitle = "Websites";
+    }
 
-	public function edit() {
-		if ($this->uri->total_arguments()==0)
-			print "cannot edit website without an ID";
-		else
-		{
-			$website = new Website_Model($this->uri->argument(1));
-			$view = new View('website/website_edit');
-			$view->metadata = $this->GetMetadataView($website);
-			$this->template->title = $this->GetEditPageTitle($website, 'Website');
-			$view->model = $website;
-			$this->template->content = $view;
-		}
-	}
+    /**
+     * Action for website/create page.
+     * Displays a page allowing entry of a new website.
+     *
+     * @todo auth and permission check
+     *       May you should rename this methode to "new". I find "create" a bit confusing
+     *       because this methode create nothing but just loads a view with empty form fields
+     */
+    public function create()
+    {
+        $this->model = ORM::factory('website');
+        $this->set_view('website/website_edit', 'Website');
+    }
 
-	public function save() {
-		if (! empty($_POST['id']))
-			$website = new Website_Model($_POST['id']);
-		else
-			$website = new Website_Model();
-		$_POST = new Validation($_POST);
-		if ($website->validate($_POST, TRUE)) {
-			url::redirect('website');
-		} else {
-			// errors are now embedded in the model
-		    $view = new View('website/website_edit');
-			$view->metadata = $this->GetMetadataView($website);
-			$this->template->title = $this->GetEditPageTitle($website, 'Website');
-			$view->model = $website;
-			$this->template->content = $view;
-		}
+    /**
+     * Edit website data
+     *
+     * @todo auth and permission check
+     */
+    public function edit()
+    {
+        if ($this->uri->total_arguments()==0)
+        {
+            // we need a general error controller
+            print "cannot edit website without an ID";
+        }
+        else
+        {
+            $this->model = new Website_Model($this->uri->argument(1));
+            $this->set_view('website/website_edit', 'Website');
+        }
+    }
 
-	}
+    /**
+     * Save website form data
+     *
+     * @todo auth and permission check
+     */
+    public function save()
+    {
+        // only proceed if a website id exists
+        if ( isset($_POST['id']) )
+        {
+            $this->model = new Website_Model( $_POST['id'] );
+        }
+        else
+        {
+            // undefined website id
+            // usually this should never be the case
+            // we need a general error controller for such cases (exception handling?)
+            url::redirect('website');
+        }
+
+        if ($this->model->saveData( $_POST ))
+        {
+            // on success reload the 'website' controller
+            url::redirect('website');
+        }
+        else
+        {
+            // if some thing is going wrong we set the view to the same page
+            $this->set_view('website/website_edit', 'Website');
+        }
+    }
+
+    /**
+     * set view
+     *
+     * @param string $name View name
+     * @param string $pagetitle Page title
+     */
+    private function set_view( $name, $pagetitle = '' )
+    {
+        // on error rest on the website_edit page
+        // errors are now embedded in the model
+        $view                    = new View( $name );
+        $view->metadata          = $this->GetMetadataView(  $this->model );
+        $this->template->title   = $this->GetEditPageTitle( $this->model, $pagetitle );
+        $view->model             = $this->model;
+        $this->template->content = $view;
+    }
 
 }
 
