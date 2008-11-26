3df9512702cd0f8dc7180d592b25ce79da4ca5c2
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-26 18:52:04 +0000

    Termlists_term updated to use Armand's code. Taxon_list likewise and switched to new save procedures.

--- trunk/application/controllers/taxon_list.php
+++ trunk/application/controllers/taxon_list.php
@@ -8,12 +8,13 @@ class Taxon_list_Controller extends Gridview_Base_Controller {
 			'title'=>'',
 			'description'=>'');
 		$this->pagetitle = "Taxon lists";
+		$this->model = ORM::factory('taxon_list');
 	}
 	public function edit($id,$page_no,$limit) {
-		$model = ORM::factory('taxon_list',$id);
+		$this->model->find($id);
 
 		// Configure the grid
-		$grid =	Gridview_Controller::factory($model,
+		$grid =	Gridview_Controller::factory($this->model,
 				$page_no,
 				$limit,
 				4);
@@ -23,20 +24,11 @@ class Taxon_list_Controller extends Gridview_Base_Controller {
 			'edit' => 'taxon_list/edit/£id£'
 		);
 
-		// Configure the metadata panel
-		$metadata = new View('templates/metadata');
-		$metadata->model = $model->find($id);
+		$vArgs = array(
+			'table' => $grid->display()
+		);
 
-		// Configure and assign variables to the view
-		$view = new View('taxon_list/taxon_list_edit');
-		$view->table = $grid->display();
-		$view->model = $model->find($id);
-
-		// Templating
-		$view->metadata = $metadata;
-		$this->template->title = "Edit ".$model->title;
-		$this->template->content = $view;
-
+		$this->setView('taxon_list/taxon_list_edit', 'Species List', $vArgs);
 	}
 	// Auxilliary function for handling Ajax requests from the edit method gridview component
 	public function edit_gv($id,$page_no,$limit) {
@@ -55,49 +47,12 @@ class Taxon_list_Controller extends Gridview_Base_Controller {
 		);
 		return $grid->display();
 	}
-	public function save() {
-		if (! empty($_POST['id'])) {
-			$taxon_list = ORM::factory('taxon_list',$_POST['id']);
-		} else {
-			$taxon_list = ORM::factory('taxon_list');
-		}
-		if ($_POST['parent_id'] == ''){
-			$_POST['parent_id'] = null;
-		}
-		if ($_POST['website_id'] == ''){
-			$_POST['website_id'] = null;
-		}
-		if ($_POST['submit'] == 'Delete'){
-			$_POST['deleted'] = 'true';
-		} else {
-			$_POST['deleted'] = 'false';
-		}
-		$_POST = new Validation($_POST);
-		if ($taxon_list->validate($_POST, true)) {
-			url::redirect('taxon_list');
-		} else {
-			$metadata = new View('templates/metadata');
-			$metadata->model = $taxon_list;
-
-			$view = new View('taxon_list/taxon_list_edit');
-			$view->metadata = $metadata;
-			$view->model = $taxon_list;
-
-			$this->template->title = "Edit ".$taxon_list->title;
-			$this->template->content = $view;
-		}
-	}
 	public function create(){
 		$parent = $this->input->post('parent_id', null);
-		$metadata = new View('templates/metadata');
-		$metadata->model = ORM::factory('taxon_list');
-		$view = new View('taxon_list/taxon_list_edit');
-		$view->metadata = $metadata;
-		$view->model = ORM::factory('taxon_list');
-		$view->model->parent_id = $parent;
-		if ($parent != null) $view->model->website_id = $view->model->parent->website_id;
-		$this->template->title = "Create new taxon_list";
-		$this->template->content = $view;
+		$this->model->parent_id = $parent;
+		if ($parent != null) $this->model->website_id = $this->model->parent->website_id;
+
+		$this->setView('taxon_list/taxon_list_edit', 'Species List');
 	}
 }
 ?>
