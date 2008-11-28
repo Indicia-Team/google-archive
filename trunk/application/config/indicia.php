8b588bc472ce564b7840f6fe90a300e8ed192ab1
armand.turpel <armand.turpel@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-11-28 14:17:05 +0000

    init of the system configuration file

---
+++ trunk/application/config/indicia.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * INDICIA
+ *
+ * Contains system information
+ *
+ * @link http://code.google.com/p/indicia/
+ *
+ * @package Indicia
+ * @license http://www.gnu.org/licenses/gpl.html GPL
+ */
+$config['system'] = array
+(
+	'setup'        => true,
+	'version'      => '0.1',
+	'name'         => '',
+	'repository'   => 'http://indicia.googlecode.com/svn/tag/version_0_1',
+	'release_date' => '2009-01-15'
+);
+
+?>
