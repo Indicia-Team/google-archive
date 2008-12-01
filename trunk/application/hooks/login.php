0eae6c2a435e0f1a8baca1a87964f0498d878bc7
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-12-01 08:17:09 +0000

    Tidy

--- trunk/application/hooks/login.php
+++ trunk/application/hooks/login.php
@@ -8,13 +8,23 @@ class login {
 		Event::add('system.routing', array($this, 'check'));
 	}
 
+	/**
+	 * When visiting any page on the site, check if the user is already logged in,
+	 * or they are visiting a page that is allowed when logged out. Otherwise,
+	 * redirect to the login page. If visiting the login page, check the browser
+	 * supports cookies.
+	 */
 	public function check()
 	{
 		// Always logged in
 		$auth = new Auth();
 		$uri = new URI();
 
-		if ($uri->segment(1) != 'login' AND $uri->segment(1) != 'logout' AND $uri->segment(1) != 'new_password' AND $uri->segment(1) != 'forgotten_password' AND ! $auth->logged_in())
+		if (	! $auth->logged_in() AND
+				$uri->segment(1) != 'login' AND
+				$uri->segment(1) != 'logout' AND
+				$uri->segment(1) != 'new_password' AND
+				$uri->segment(1) != 'forgotten_password')
 		{
 			$_SESSION['requested_page'] = $uri->string();
 			url::redirect('login');
