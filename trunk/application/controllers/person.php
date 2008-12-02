470a131ce8ccf0dee1d0bd2d1b4441aa55dbcec9
vanbreda@btinternet.com <vanbreda@btinternet.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-12-02 13:29:42 +0000

    Refactor of User and Person pages. Alterations to gridviews. Can no longer drill through from person to user, and from user to person is controlled from the user gridview.

--- trunk/application/controllers/person.php
+++ trunk/application/controllers/person.php
@@ -3,7 +3,7 @@
 class Person_Controller extends Gridview_Base_Controller {
 
 	public function __construct() {
-		parent::__construct('person', 'gv_person', 'person/index');
+		parent::__construct('person', 'person', 'person/index');
 		$this->columns = array(
 			'first_name'=>''
 			,'surname'=>''
@@ -13,46 +13,32 @@ class Person_Controller extends Gridview_Base_Controller {
 			,'is_core_user'=>''
 		);
 		$this->pagetitle = "People";
+		$this->model = new Person_Model();
 	}
 
-	protected function create_user_button($extras='')
-	{
-		return form::submit('submit', 'Create User Details', $extras);
-	}
-	
-	protected function edit_user_button($extras='')
-	{
-		return form::submit('submit', 'Edit User Details', $extras);
-	}
-	
 	protected function return_url($return_url)
 	{
 		return '<input type="hidden" name="return_url" id="return_url" value="'.html::specialchars($return_url).'" />';
 	}
 
-	protected function disable_button($disable_button)
-	{
-		return $disable_button ? '<input type="hidden" name="disable_button" id="disable_button" value="YES" />' : '';
-	}
-	
 	/**
 	 * Action for person/create page.
 	 * Displays a page allowing entry of a new person.
 	 */
 	public function create() {
-		$person = ORM::factory('person');
-		$view = new View('person/person_edit');
-		$view->model = $person;
-		$view->metadata = $this->GetMetadataView($person);
-		$this->template->title = $this->GetEditPageTitle($person, 'Person');
-		$view->return_url = ''; // will jump back to the gridview on submit
-		// There are issues with the ID not being backfilled into the model when a new record has been created.
-		// For this reason the Create User details is disabled.
-		$view->user_details_button = $this->create_user_button('disabled="disabled"');
-		$view->disable_button = $this->disable_button(TRUE);
-		$this->template->content = $view;
+		$this->setView('person/person_edit', 'Person',
+					array('return_url' => '')); // will jump back to the gridview on submit
 	}
 
+	/**
+	 * Action for person/create page.
+	 * Displays a page allowing entry of a new person.
+	 */
+	public function create_from_user() {
+		$this->setView('person/person_edit', 'Person',
+					array('return_url' => $this->return_url('user'))); // will jump back to the user gridview on submit
+	}
+	
 	/**
 	 * Action for person/edit page.
 	 * Displays a page allowing modification of an existing person.
@@ -60,21 +46,17 @@ class Person_Controller extends Gridview_Base_Controller {
 	 * 1) From the gridview
 	 * 2) Direct URL
 	 */
-	public function edit() {
-		if ($this->uri->total_arguments()==0)
-			print "cannot edit person without a Person ID";
-		else
+	public function edit($id = NULL) {
+        if ($id == null)
+        {
+            // we need a general error controller
+            print "You cannot use the edit person functionality without an ID";
+        }
+        else
 		{
-			$person = new Person_Model($this->uri->argument(1));
-			$view = new View('person/person_edit');
-			$view->model = $person;
-			$view->metadata = $this->GetMetadataView($person);
-			$this->template->title = $this->GetEditPageTitle($person, 'Person');
-			$view->return_url = ''; // will jump back to the gridview on submit
-			$user = ORM::factory('user', array('person_id' => $person->id));
-			$view->user_details_button = $user->loaded ? $this->edit_user_button() : $this->create_user_button();
-			$view->disable_button = $this->disable_button(FALSE);
-			$this->template->content = $view;
+			$this->model = new Person_Model($id);
+			$this->setView('person/person_edit', 'Person',
+					array('return_url' => '')); // will jump back to the gridview on submit
 		}
 	}
 
@@ -82,72 +64,38 @@ class Person_Controller extends Gridview_Base_Controller {
 	 * Subsiduary Action for person/edit page.
 	 * Displays a page allowing modification of an existing person.
 	 * This is called from a User Record.
-	 * When called from User we want:
-	 * A) To return back to the User form on submission for that person
-	 * B) We don't want to allow the drilling back to the user - ie we need to disable the relevant button.
+	 * When called from User we want to return back to the User gridview on submission for that person
 	 */
-	public function edit_from_user() {
-		if ($this->uri->total_arguments()==0)
-			print "cannot edit person through edit_from_user() without a Person ID";
-		else
+	public function edit_from_user($id = NULL) {
+        if ($id == null)
+        {
+            // we need a general error controller
+            print "You cannot edit a person through edit_from_user() without a Person ID";
+        }
+        else
 		{
-			$person = new Person_Model($this->uri->argument(1));
-			$view = new View('person/person_edit');
-			$view->model = $person;
-			$view->metadata = $this->GetMetadataView($person);
-			$this->template->title = $this->GetEditPageTitle($person, 'Person');
-			$user = ORM::factory('user', array('person_id' => $person->id));
-			$view->return_url = $user->loaded ? $this->return_url('user/edit/'.$user->id): '';
-			$view->user_details_button = $this->edit_user_button('disabled="disabled"');				
-			$view->disable_button = $this->disable_button(TRUE);
-			$this->template->content = $view;
+			$this->model = new Person_Model($id);
+			$this->setView('person/person_edit', 'Person',
+					array('return_url' => $this->return_url('user')));
 		}
 	}
 
-	
-	public function save() {
-		if (! empty($_POST['id']))
-			$person = new Person_Model($_POST['id']);
-		else
-			$person = new Person_Model();
-			
-		$_POST = new Validation($_POST);
-		if ($person->validate($_POST, TRUE)) {
-			
-			if(isset($_POST['return_url'])) 
-				url::redirect($_POST['return_url']);
-			else if ($_POST['submit'] == 'Edit User Details'){
-				url::redirect('user/edit_from_person/'.$person->id);
-			} else if ($_POST['submit'] == 'Create User Details'){
-				url::redirect('user/create/'.$person->id);
-			} else
-				// For a successful submission, just redisplay the gridview
-				url::redirect('person');
-				
-		} else {
-			// errors are now embedded in the model
-			$view = new View('person/person_edit');
-			$view->model = $person;
-			$view->metadata = $this->GetMetadataView($person);
-			$this->template->title = $this->GetEditPageTitle($person, 'Person');
-			$view->return_url = isset($_POST['return_url']) ? $this->return_url($_POST['return_url']) : '';
-			if ( isset($_POST['disable_button'] ) ) {
-				$extras='disabled="disabled"';
-				$view->disable_button = $this->disable_button(TRUE);
-			} else {
-				$extras='';
-				$view->disable_button = $this->disable_button(FALSE);
-			}
-			$view->user_details_button = $this->create_user_button($extras);				
-			if ( isset($_POST['id'])) {
-				$user = ORM::factory('user', array('person_id' => $person->id));
-				if ( $user->loaded )
-					$view->edit_user_button = $this->edit_user_button($extras);
-			}
-			$this->template->content = $view;
-		}
-	}
+	/**
+     * Returns to the index view for this controller.
+     */
+    protected function submit_succ($id) {
+        syslog(LOG_DEBUG, "Submitted record ".$id." successfully.");
+		if(isset($_POST['return_url'])) 
+			url::redirect($_POST['return_url']);
 
+		url::redirect($this->model->object_name);
+    }
+
+	protected function submit_fail() {
+		$this->setView('person/person_edit', 'Person',
+			array('return_url' => isset($_POST['return_url']) ? $this->return_url($_POST['return_url']) : ''));
+	}
+    
 }
 
 ?>
