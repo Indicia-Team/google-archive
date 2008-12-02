f956e6fa2f5ca0571b7422a284eda041ef6ee7b4
vanbreda <vanbreda@btinternet.com>
2008-12-02 14:23:54 +0000

    Enable request to send forgotten password email from users gridview.

--- trunk/application/controllers/forgotten_password.php
+++ trunk/application/controllers/forgotten_password.php
@@ -70,4 +70,51 @@ class Forgotten_Password_Controller extends Indicia_Controller {
 			$this->template->content->message = 'An email providing a link which will allow your password to be reset has been sent to the specified email address, or if a Username was provided, to the registered email address for that User.<br />';			
 		}
 	}
+
+	public function send_from_user($id = null)
+	{
+		$email_config = Kohana::config('email');
+		
+		$this->template->title = 'Forgotten Password Email Request';
+		$this->template->content = new View('login/login_message');	
+		$this->template->content->message = 'You are already logged in.<br />';
+		$this->template->content->link_to_home = 'YES';
+		$person = ORM::factory('person', $id);
+		if ( ! $person->loaded )
+		{
+			$this->template->content->message = 'Invalid Person ID';
+			return;
+		}
+		$user = ORM::factory('user', array('person_id' => $id));
+		if ( ! $user->loaded )
+		{
+			$this->template->content->message = 'No user details have been set up for this Person';
+			return;
+		}
+		if ( is_null($user->core_role_id) )
+		{
+			$this->template->content->message = $user->username.' does not have permission to log on to this website';
+			return;
+		}
+		$link_code = $this->auth->hash_password($user->username);
+		$user->__set('forgotten_password_key', $link_code);
+		$user->save();
+		$swift = email::connect();
+		$message = new Swift_Message($email_config['forgotten_passwd_title'],
+	                             View::factory('templates/forgotten_password_email_2')->set(array('server' => $email_config['server_name'], 'new_password_link' => '<a href="'.url::site().'new_password/email/'.$link_code.'">'.url::site().'new_password/email/'.$link_code.'</a>')),
+	                             'text/html');
+		$recipients = new Swift_RecipientList();
+		$recipients->addTo($person->email_address, $person->first_name.' '.$person->surname);
+
+		try
+		{
+			$swift->send($message, $recipients, $email_config['address']);
+		}
+		catch (Swift_ConnectionException $e)
+		{
+			throw new Kohana_User_Exception('swift.general_error', $e->getMessage());
+		}
+		url::redirect('user');
+	}
+	
 }
