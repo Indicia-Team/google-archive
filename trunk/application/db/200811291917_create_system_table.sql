6b82295385ac3fa7b9248e1a19c5a6ddd548c951
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-12-02 12:12:26 +0000

    System pk now called id.

--- trunk/application/db/200811291917_create_system_table.sql
+++ trunk/application/db/200811291917_create_system_table.sql
@@ -1,29 +1,17 @@
 DROP TABLE IF EXISTS "system";
 
 CREATE TABLE "system" (
-    id_system integer NOT NULL,
+    id serial NOT NULL,
     "version" character varying(10) DEFAULT ''::character varying NOT NULL,
     "name" character varying(30) DEFAULT ''::character varying NOT NULL,
     repository character varying(150) DEFAULT ''::character varying NOT NULL,
-    release_date date
+    release_date date,
+    CONSTRAINT pk_system PRIMARY KEY (id)
 );
 
-DROP SEQUENCE IF EXISTS system_id_system_seq1;
-
-CREATE SEQUENCE system_id_system_seq1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE system_id_system_seq1 OWNED BY system.id_system;
-
-SELECT pg_catalog.setval('system_id_system_seq1', 1, true);
-
-ALTER TABLE "system" ALTER COLUMN id_system SET DEFAULT nextval('system_id_system_seq1'::regclass);
-
-ALTER TABLE ONLY "system"
-    ADD CONSTRAINT id_system PRIMARY KEY (id_system);
+COMMENT ON COLUMN system.version IS 'Version number.';
+COMMENT ON COLUMN system.name IS 'Version name.';
+COMMENT ON COLUMN system.repository IS 'SVN repository path.';
+COMMENT ON COLUMN system.release_date IS 'Release date for version.';
 
 INSERT INTO "system" ("id_system", "version", "name", "repository", "release_date") VALUES (1, '0.1', '', 'http://indicia.googlecode.com/svn/tag/version_0_1', '2009-01-15');
