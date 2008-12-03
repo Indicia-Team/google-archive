1313b2ec3585c911e28a55a40a9e48294f796630
armand.turpel <armand.turpel@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-12-03 16:01:46 +0000

    bug in insert; changed id_system to id

--- trunk/application/db/200811291917_create_system_table.sql
+++ trunk/application/db/200811291917_create_system_table.sql
@@ -14,4 +14,4 @@ COMMENT ON COLUMN system.name IS 'Version name.';
 COMMENT ON COLUMN system.repository IS 'SVN repository path.';
 COMMENT ON COLUMN system.release_date IS 'Release date for version.';
 
-INSERT INTO "system" ("id_system", "version", "name", "repository", "release_date") VALUES (1, '0.1', '', 'http://indicia.googlecode.com/svn/tag/version_0_1', '2009-01-15');
+INSERT INTO "system" ("id", "version", "name", "repository", "release_date") VALUES (1, '0.1', '', 'http://indicia.googlecode.com/svn/tag/version_0_1', '2009-01-15');
