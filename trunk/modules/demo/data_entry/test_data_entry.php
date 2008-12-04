06b1cb0fac702b3687c8e5c1241dc1335f2ceb08
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2008-12-04 13:32:51 +0000

    Demo page now has a datepicker. Todo - close datepicker if manual data entry starts.

--- trunk/modules/demo/data_entry/test_data_entry.php
+++ trunk/modules/demo/data_entry/test_data_entry.php
@@ -1,6 +1,17 @@
 <html>
 <head>
 <title>Indicia external site data entry test page</title>
+<link rel="stylesheet" href="../../../media/css/ui.datepicker.css" type="text/css" media="screen">
+<script type="text/javascript" src="../../../media/js/jquery-1.2.6.js"></script>
+<script type="text/javascript" src="../../../media/js/ui.core.js"></script>
+<script type="text/javascript" src="../../../media/js/ui.datepicker.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$('#basics').datepicker({constrainInput: false});
+})
+</script>
+
+
 </head>
 <body>
 <h1>Indicia Data entry test</h1>
@@ -33,7 +44,9 @@
 ?>
 <form method="post">
 <label for="date">Date:</label>
-<input name="date" /><br />
+<input type="text" size="30" value="click here" id="basics"/>
+<style type="text/css">.embed + img { position: relative; left: -21px; top: -1px; }</style>
+<br />
 <label for="entered_sref">Spatial Reference:</label>
 <input name="entered_sref" /><br />
 <input type="hidden" value="osgb" name="entered_sref_system">
