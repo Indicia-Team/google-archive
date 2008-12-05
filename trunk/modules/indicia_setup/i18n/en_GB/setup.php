395dd0430096e1d3a8f40d1a2034c98112b2cad3
armand.turpel <armand.turpel@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-12-05 15:34:58 +0000

    first working version of the setup. very very alpha!

--- trunk/modules/indicia_setup/i18n/en_GB/setup.php
+++ trunk/modules/indicia_setup/i18n/en_GB/setup.php
@@ -3,7 +3,7 @@
 $lang = array
 (
 	'title'       => 'Indicia Setup',
-	'description' => 'Before continue here you must first create a postgresql database.',
+	'description' => 'Before continue here you must first create a postgresql database and may a schema.',
 	'database'    => 'Database',
 	'db_schema'   => 'Schema',
 	'db_host'     => 'Host',
@@ -21,6 +21,11 @@ $lang = array
 	'error_config_folder'     => 'The config folder must be writeable by php scripts:',
 	'error_upload_folder'     => 'The upload folder must be writeable by php scripts:',
 	'error_db_file'           => 'The indicia setup sql file must be readable by php scripts:',
+	'error_db_connect'        => 'Could not connect to the database. Please verify database connection data.',
+	'error_db_setup'          => 'Setup failed. Rollback database transactions.',
+	'error_db_database_config' => 'Setup failed. Rollback database transactions. Could not write /application/config/database.php file. Please check file write permission.',
+	'error_db_indicia_config'  => 'Setup failed. Rollback database transactions. Could not create /application/config/indicia.php file. Please check file write permission.',
+	'error_remove_folder'      => 'For continuing with the setup you have to remove or rename the config file /application/config/indicia.php',
 	'error_file_read_permission'      => 'The following files must be readable by php scripts',
 	'error_chars_not_allowed'         => 'wrong chars',
 	'error_no_postgis'                => 'Postgis not installed',
