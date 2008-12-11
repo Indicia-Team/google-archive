5bb5810bd05d73c754bcd62bac0e3148c7ce3b1c
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2008-12-11 14:22:18 +0000

    Attributes script revisions.

--- trunk/application/db/200812111221_attributes.sql
+++ trunk/application/db/200812111221_attributes.sql
@@ -51,7 +51,7 @@ COMMENT ON COLUMN occurrence_attributes_websites.restrict_to_survey_id IS 'Forei
 ALTER TABLE occurrence_attributes_websites
   ADD CONSTRAINT fk_occurrence_attributes_websites_survey FOREIGN KEY (restrict_to_survey_id)
       REFERENCES surveys (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION;      
+      ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 CREATE INDEX fki_occurrence_attributes_websites_survey
   ON occurrence_attributes_websites
@@ -65,7 +65,7 @@ COMMENT ON COLUMN sample_attributes_websites.restrict_to_survey_id IS 'Foreign k
 ALTER TABLE sample_attributes_websites
   ADD CONSTRAINT fk_sample_attributes_websites_survey FOREIGN KEY (restrict_to_survey_id)
       REFERENCES surveys (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION;      
+      ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 CREATE INDEX fki_sample_attributes_websites_survey
   ON sample_attributes_websites
@@ -79,10 +79,43 @@ COMMENT ON COLUMN location_attributes_websites.restrict_to_survey_id IS 'Foreign
 ALTER TABLE location_attributes_websites
   ADD CONSTRAINT fk_location_attributes_websites_survey FOREIGN KEY (restrict_to_survey_id)
       REFERENCES surveys (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION;      
+      ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 CREATE INDEX fki_locationattributes_websites_survey
   ON location_attributes_websites
   USING btree
   (restrict_to_survey_id);
 
+-- Allow attributes to be flagged as multi-value.
+
+ALTER TABLE occurrence_attributes ADD COLUMN multi_value boolean;
+ALTER TABLE occurrence_attributes ALTER COLUMN multi_value SET STORAGE PLAIN;
+ALTER TABLE occurrence_attributes ALTER COLUMN multi_value SET DEFAULT false;
+COMMENT ON COLUMN occurrence_attributes.multi_value IS 'Does this attribute allow multiple values? If set to true, then multiple values for this attribute can be stored against a single record.';
+
+ALTER TABLE sample_attributes ADD COLUMN multi_value boolean;
+ALTER TABLE sample_attributes ALTER COLUMN multi_value SET STORAGE PLAIN;
+ALTER TABLE sample_attributes ALTER COLUMN multi_value SET DEFAULT false;
+COMMENT ON COLUMN sample_attributes.multi_value IS 'Does this attribute allow multiple values? If set to true, then multiple values for this attribute can be stored against a single record.';
+
+ALTER TABLE location_attributes ADD COLUMN multi_value boolean;
+ALTER TABLE location_attributes ALTER COLUMN multi_value SET STORAGE PLAIN;
+ALTER TABLE location_attributes ALTER COLUMN multi_value SET DEFAULT false;
+COMMENT ON COLUMN location_attributes.multi_value IS 'Does this attribute allow multiple values? If set to true, then multiple values for this attribute can be stored against a single record.';
+
+-- Allow attributes to be flagged as public
+
+ALTER TABLE occurrence_attributes ADD COLUMN public boolean;
+ALTER TABLE occurrence_attributes ALTER COLUMN public SET STORAGE PLAIN;
+ALTER TABLE occurrence_attributes ALTER COLUMN public SET DEFAULT false;
+COMMENT ON COLUMN occurrence_attributes.public IS 'Flag set to true if this attribute is available for selection and use by any website. If false the attribute is only available for use in the website which created it.';
+
+ALTER TABLE sample_attributes ADD COLUMN public boolean;
+ALTER TABLE sample_attributes ALTER COLUMN public SET STORAGE PLAIN;
+ALTER TABLE sample_attributes ALTER COLUMN public SET DEFAULT false;
+COMMENT ON COLUMN sample_attributes.public IS 'Flag set to true if this attribute is available for selection and use by any website. If false the attribute is only available for use in the website which created it.';
+
+ALTER TABLE location_attributes ADD COLUMN public boolean;
+ALTER TABLE location_attributes ALTER COLUMN public SET STORAGE PLAIN;
+ALTER TABLE location_attributes ALTER COLUMN public SET DEFAULT false;
+COMMENT ON COLUMN location_attributes.public IS 'Flag set to true if this attribute is available for selection and use by any website. If false the attribute is only available for use in the website which created it.';
