0a68ebd2f53ff0248fe2f7699cc1cae41cea2603
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-12-11 17:35:01 +0000

    Occurrence attribute model

---
+++ trunk/application/models/occurrence_attribute.php
@@ -0,0 +1,21 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Occurrence_Attribute_Model extends ORM {
+
+	protected $belongs_to = array('created_by'=>'user', 'updated_by'=>'user', 'termlist');
+
+	protected $has_many = array(
+		'occurrence_attributes_values',
+		);
+
+	protected $has_and_belongs_to_many = array('websites');
+
+	protected $search_field='caption';
+
+	public function validate(Validation $array, $save = FALSE) {
+		// uses PHP trim() to remove whitespace from beginning and end of all fields before validation
+		$array->pre_filter('trim');
+		return parent::validate($array, $save);
+	}
+
+}
