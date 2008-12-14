16f1f4468e1dadd72064ceed7fc14df847477c28
armand.turpel <armand.turpel@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-12-14 15:04:53 +0000

    striped owner opal

--- trunk/application/db/200812091204_list_people_caption.sql
+++ trunk/application/db/200812091204_list_people_caption.sql
@@ -3,5 +3,3 @@ DROP VIEW list_people;
 CREATE OR REPLACE VIEW list_people AS 
  SELECT p.id, p.first_name, p.surname, p.initials, p.first_name||' '||p.surname as caption
    FROM people p;
-
-ALTER TABLE list_people OWNER TO opal;
