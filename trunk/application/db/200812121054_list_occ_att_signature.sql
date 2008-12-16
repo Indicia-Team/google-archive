6a0ccae739c83a6b7eff5b96a5e3688edd0acf4c
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-12-16 15:12:52 +0000

    Removed OPAL role

--- trunk/application/db/200812121054_list_occ_att_signature.sql
+++ trunk/application/db/200812121054_list_occ_att_signature.sql
@@ -2,9 +2,8 @@
 
 DROP VIEW list_occurrence_attributes;
 
-CREATE OR REPLACE VIEW list_occurrence_attributes AS 
+CREATE OR REPLACE VIEW list_occurrence_attributes AS
  SELECT oa.id, oa.caption, oa.data_type, oa.termlist_id, oa.multi_value, oaw.website_id, (((oa.id || '|'::text) || oa.data_type::text) || '|'::text || COALESCE(oa.termlist_id::text, ''::text))  AS signature
    FROM occurrence_attributes oa
    LEFT JOIN occurrence_attributes_websites oaw ON oaw.occurrence_attribute_id = oa.id;
 
-ALTER TABLE list_occurrence_attributes OWNER TO opal;
