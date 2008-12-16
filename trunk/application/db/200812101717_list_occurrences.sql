91732ddd44c50f373b292652f9ba9fa1afd8ca32
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2008-12-16 15:11:55 +0000

    Removed Role of OPAL

--- trunk/application/db/200812101717_list_occurrences.sql
+++ trunk/application/db/200812101717_list_occurrences.sql
@@ -1,6 +1,4 @@
-DROP VIEW list_occurrences;
-
-CREATE OR REPLACE VIEW list_occurrences AS 
+CREATE OR REPLACE VIEW list_occurrences AS
  SELECT su.title as "survey", l.name AS "location", (d.first_name::text || ' '::text) || d.surname::text AS determiner, t.taxon
    FROM occurrences o
    JOIN samples s ON o.sample_id = s.id
@@ -9,5 +7,3 @@ CREATE OR REPLACE VIEW list_occurrences AS
    JOIN taxa_taxon_lists ttl ON o.taxa_taxon_list_id = ttl.id
    JOIN taxa t ON ttl.taxon_id = t.id
    JOIN surveys su ON s.survey_id = su.id;
-
-ALTER TABLE list_occurrences OWNER TO opal;
