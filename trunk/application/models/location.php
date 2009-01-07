e7376d72a12e1d86d84372410f697e610bd960b3
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2009-01-07 20:56:09 +0000

    Locations bugfix

--- trunk/application/models/location.php
+++ trunk/application/models/location.php
@@ -15,7 +15,7 @@ class Location_Model extends ORM_Tree {
 		// uses PHP trim() to remove whitespace from beginning and end of all fields before validation
 		$array->pre_filter('trim');
 		$array->add_rules('name', 'required');
-		$system = $orig_values['entered_sref_system'];
+		$system = $orig_values['centroid_sref_system'];
 		$array->add_rules('centroid_sref', 'required', "sref[$system]");
 		$array->add_rules('centroid_sref_system', 'required', 'sref_system');
 
