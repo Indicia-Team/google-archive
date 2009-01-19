e384c7d0cccd8171a40ea4c772c1f8e1c5064c61
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2009-01-19 15:23:27 +0000

    Added species checklist to demo index.

--- trunk/modules/demo/index.php
+++ trunk/modules/demo/index.php
@@ -6,7 +6,8 @@
 <h1>Indicia demonstrations</h1>
 <p>The following list of demonstration pages illustrate service based Indicia functionality.</p>
 <ol>
-<li><a href="data_entry/test_data_entry.php">Demonstration data entry page</a></li>
+<li><a href="data_entry/test_data_entry.php">Demonstration basic data entry page</a></li>
+<li><a href="data_entry/species_checklist.php">Demonstration checklist based data entry page</a></li>
 <li><a href="map.php">Demonstration distribution map</a></li>
 <li><a href="valid.php">Simple demonstration of validation</a></li>
 <li><a href="../../index.php/services/data/location?mode=xml&view=detail">List the locations as XML by calling the Data service</a></li>
