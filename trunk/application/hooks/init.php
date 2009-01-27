b72d30e5693d85fcf0b8b8e4d47c91d08c6a281b
armand.turpel <armand.turpel@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-01-27 13:06:00 +0000

    indicia should now work on any db schema

--- trunk/application/hooks/init.php
+++ trunk/application/hooks/init.php
@@ -2,21 +2,65 @@
 
 class Indicia
 {
-	public function init()
-	{
-		set_error_handler(array('Indicia', 'indicia_exception_handler'));
-	}
+    public function init()
+    {
+        set_error_handler(array('Indicia', 'indicia_exception_handler'));
+    }
 
-	/**
-	 * Convert PHP errors to exceptions so that they can be handled nicely.
-	 */
-	public static function indicia_exception_handler($errno, $errstr, $errfile, $errline)
-	{
-		throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
-	}
+    /**
+     * Convert PHP errors to exceptions so that they can be handled nicely.
+     */
+    public static function indicia_exception_handler($errno, $errstr, $errfile, $errline)
+    {
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+
+    /**
+     * set schema search path for postgresql
+     */
+    public static function set_schema_search_path()
+    {
+        $db = Database::instance();
+
+        $_schema = Kohana::config('database.default.schema');
+
+        if(!empty($_schema))
+        {
+            $result = $db->query('SET search_path TO ' . $_schema . ', public, pg_catalog');
+        }
+    }
+
+    /**
+     * check if setup wasnt done
+     */
+    public static function _check_setup()
+    {
+        $uri = URI::instance();
+
+        if($uri->segment(1) == 'setup')
+        {
+            return;
+        }
+
+        // load indicia system information
+        //
+        $system = Kohana::config('indicia.system', false, false);
+
+        // check if the general system setup was done
+        // The setup wasnt done if the indicia.php config file dosent exists.
+        //
+        if($system === null)
+        {
+            unset($_COOKIE);
+            url::redirect('setup');
+        }
+    }
 
 }
 
-Event::add('system.ready', array('Indicia', 'Init'));
+Event::add('system.routing', array('Indicia', '_check_setup'));
+Event::add('system.ready',   array('Indicia', 'Init'));
+Event::add('system.routing', array('Indicia', 'set_schema_search_path'));
+
 
 ?>
