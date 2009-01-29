1872f4d28981a20b21121d2d463095202529dbb5
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-01-29 08:18:13 +0000

    Fixed logic for users view to retrieve deleted flag properly.

--- trunk/application/db/200901282041_deleted_views.sql
+++ trunk/application/db/200901282041_deleted_views.sql
@@ -8,8 +8,8 @@ CREATE OR REPLACE VIEW gv_surveys AS
 DROP VIEW gv_users;
 
 CREATE OR REPLACE VIEW gv_users AS 
- SELECT p.id AS person_id, COALESCE(p.first_name::text || ' '::text, ''::text) || p.surname::text AS name, u.id, u.username, cr.title AS core_role, u.deleted
+ SELECT p.id AS person_id, COALESCE(p.first_name::text || ' '::text, ''::text) || p.surname::text AS name, u.id, u.username, cr.title AS core_role, p.deleted
    FROM people p
-   LEFT JOIN users u ON p.id = u.person_id
+   LEFT JOIN users u ON p.id = u.person_id and u.deleted='f'
    LEFT JOIN core_roles cr ON u.core_role_id = cr.id
   WHERE p.email_address IS NOT NULL;
