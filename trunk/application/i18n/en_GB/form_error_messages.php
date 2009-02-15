04850105c12eef146d9a6b4da9d13fcf5906f4e6
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-02-15 12:12:08 +0000

    added an error message

--- trunk/application/i18n/en_GB/form_error_messages.php
+++ trunk/application/i18n/en_GB/form_error_messages.php
@@ -45,6 +45,10 @@ $lang = array (
 	'taxon_id' => array (
 		'default' => 'Unable to create a valid taxon entry',
 	),
+	'taxon_list_id' => array (
+		'default' => 'Invalid input.',
+		'required' => 'The taxon list must be specified.',
+	),
 	'username' => Array (
 		'required' => 'The username cannot be blank.',
 		'length' => 'The username must be between 7 and 30 letters in length.',
