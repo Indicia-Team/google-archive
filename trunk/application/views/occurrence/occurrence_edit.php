77990e9f192776762b472383cf23cd15e41a673d
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-02-16 18:02:17 +0000

    Occurrence core forms

--- trunk/application/views/occurrence/occurrence_edit.php
+++ trunk/application/views/occurrence/occurrence_edit.php
@@ -1,12 +1,65 @@
+<?php echo html::script(array(
+	'media/js/jquery.ajaxQueue.js',
+	'media/js/jquery.bgiframe.min.js',
+	'media/js/thickbox-compressd.js',
+	'media/js/jquery.autocomplete.js'
+), FALSE); ?>
+<script type="text/javascript" >
+$(document).ready(function() {
+	$("input#determiner").autocomplete("<?php echo url::site() ?>services/data/person", {
+		minChars : 1,
+		mustMatch : true,
+		extraParams : {
+			orderby : "caption",
+			mode : "json",
+			deleted : 'false'
+		},
+		parse: function(data) {
+			var results = [];
+			var obj = JSON.parse(data);
+			$.each(obj, function(i, item) {
+				results[results.length] = {
+					'data' : item,
+					'value' : item.id,
+					'result' : item.caption };
+			});
+			return results;
+		},
+		formatItem: function(item) {
+			return item.caption;
+		},
+		formatResult: function(item) {
+			return item.id;
+		}
+	});
+	$("input#determiner").result(function(event, data){
+alert(data.id);
+		$("input#determiner_id").attr('value', data.id);
+	});
+});
+</script>
 <form class="cmxform"  name='editList' action="<?php echo url::site().'occurrence/save' ?>" method="POST">
 <input type="hidden" name="id" id="id" value="<?php echo html::specialchars($model->id); ?>" />
 <fieldset>
 <legend>Occurrence Details</legend>
+<?php print form::hidden('website_id'); ?>
 <ol>
 <li>
 <label for='taxon'>Taxon:</label>
-<input type='text' name='taxon' id='taxon' value='<?php echo $model->taxa_taxon_list->taxon->taxon; ?>' />
-<?php echo html::error_message($model->taxa_taxon_list->taxon->getError('taxon')); ?>
+<?php print form::input('taxon', $model->taxa_taxon_list->taxon->taxon); 
+print form::hidden('taxa_taxon_list_id', $model->taxa_taxon_list_id); 
+echo html::error_message($model->getError('taxa_taxon_list_id')); ?>
+</li>
+<li>
+<label for='date'>Date:</label>
+<?php print form::input('date');
+echo html::error_message($model->taxa_taxon_list->taxon->getError('taxon')); ?>
+</li>
+<li>
+<label for='determiner'>Determiner:</label>
+<?php print form::input('determiner');
+print form::hidden('determiner_id');
+echo html::error_message($model->getError('determiner_id')); ?>
 </li>
 </ol>
 </fieldset>
