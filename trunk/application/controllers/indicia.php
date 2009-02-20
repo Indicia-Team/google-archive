b0bd0bc1b51cfb24ccd7f4f117ade66590ad6335
armand.turpel <armand.turpel@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-02-20 10:59:37 +0000

    bug fix of exceptions. better error handling. error displaying depends on the display_error settings in the main config file.

--- trunk/application/controllers/indicia.php
+++ trunk/application/controllers/indicia.php
@@ -28,38 +28,38 @@ class Indicia_Controller extends Template_Controller {
        'Lookup Lists' => array
        (
        'Species Lists'=>'taxon_list',
-	'Taxon Groups'=>'taxon_group',
-	'Term Lists'=>'termlist',
-	'Locations'=>'location',
-	'Surveys'=>'survey',
-	'People'=>'person'
-	),
-	'Custom Attributes' => array
-	(
-	'Occurrence Attributes'=>'occurrence_attribute',
-	'Sample Attributes'=>'sample_attribute',
-	'Location Attributes'=>'location_attribute'
-	),
-	'Entered Data' => array
-	(
-	'Occurrences' => 'occurrence',
-	'Samples' => 'sample'
-	),
-	'Admin' => array
-	(
-	'Users'=>'user',
-	'Websites'=>'website',
-	'Languages'=>'language'
-	),
-	'Logged in as '.$_SESSION['auth_user']->username => array
-	(
-	'Set New Password' => 'new_password',
-	'Logout'=>'logout'
-	)
-	);
-	if(!$this->auth->logged_in('CoreAdmin'))
-	unset($menu['Admin']);
-	$this->template->menu = $menu;
+    'Taxon Groups'=>'taxon_group',
+    'Term Lists'=>'termlist',
+    'Locations'=>'location',
+    'Surveys'=>'survey',
+    'People'=>'person'
+    ),
+    'Custom Attributes' => array
+    (
+    'Occurrence Attributes'=>'occurrence_attribute',
+    'Sample Attributes'=>'sample_attribute',
+    'Location Attributes'=>'location_attribute'
+    ),
+    'Entered Data' => array
+    (
+    'Occurrences' => 'occurrence',
+    'Samples' => 'sample'
+    ),
+    'Admin' => array
+    (
+    'Users'=>'user',
+    'Websites'=>'website',
+    'Languages'=>'language'
+    ),
+    'Logged in as '.$_SESSION['auth_user']->username => array
+    (
+    'Set New Password' => 'new_password',
+    'Logout'=>'logout'
+    )
+    );
+    if(!$this->auth->logged_in('CoreAdmin'))
+    unset($menu['Admin']);
+    $this->template->menu = $menu;
     } else
       $this->template->menu = array();
   }
@@ -108,7 +108,7 @@ class Indicia_Controller extends Template_Controller {
     $view->model             = $this->model;
 
     foreach ($viewArgs as $arg => $val) {
-	$view->set($arg, $val);
+    $view->set($arg, $val);
       }
       $this->template->content = $view;
   }
@@ -138,36 +138,36 @@ class Indicia_Controller extends Template_Controller {
        // Iterate through the array
        foreach ($array as $a => $b)
        {
-	 // Check whether this is a fk placeholder
-	 if (substr($a,0,3) == 'fk_'
-	   && $fkLink)
-	 {
-	   // Generate a foreign key instance
-	   $sa['fkFields'][$a] = array
-	   (
-	   // Foreign key id field is table_id
-	   'fkIdField' => substr($a,3)."_id",
-	   'fkTable' => substr($a,3),
-	   'fkSearchField' =>
-	   ORM::factory(substr($a,3))->get_search_field(),
-	   'fkSearchValue' => $b
-	   );
-	   // Determine the foreign table name
-	   $m = ORM::factory($id);
-	   if (array_key_exists(substr($a,3), $m->belongs_to))
-	   {
-	     $sa['fkFields'][$a]['fkTable'] = $m->belongs_to[substr($a,3)];
-	   } else if ($m instanceof ORM_Tree && substr($a,3) == 'parent') {
-	     $sa['fkFields'][$a]['fkTable'] = $id;
-	 }
+     // Check whether this is a fk placeholder
+     if (substr($a,0,3) == 'fk_'
+       && $fkLink)
+     {
+       // Generate a foreign key instance
+       $sa['fkFields'][$a] = array
+       (
+       // Foreign key id field is table_id
+       'fkIdField' => substr($a,3)."_id",
+       'fkTable' => substr($a,3),
+       'fkSearchField' =>
+       ORM::factory(substr($a,3))->get_search_field(),
+       'fkSearchValue' => $b
+       );
+       // Determine the foreign table name
+       $m = ORM::factory($id);
+       if (array_key_exists(substr($a,3), $m->belongs_to))
+       {
+         $sa['fkFields'][$a]['fkTable'] = $m->belongs_to[substr($a,3)];
+       } else if ($m instanceof ORM_Tree && substr($a,3) == 'parent') {
+         $sa['fkFields'][$a]['fkTable'] = $id;
+     }
        }
        else
        {
-	 // This should be a field in the model.
-	 // Add a new field to the save array
-	 $sa['fields'][$a] = array(
-	 // Set the value
-	 'value' => $b);
+     // This should be a field in the model.
+     // Add a new field to the save array
+     $sa['fields'][$a] = array(
+     // Set the value
+     'value' => $b);
        }
   }
   return $sa;
@@ -257,9 +257,17 @@ private function check_for_upgrade()
 
     // upgrade to version $new_system['version']
     //
-    if(false === $upgrade->run($db_system->getVersion(), $new_system))
+    if(true !== ($result = $upgrade->run($db_system->getVersion(), $new_system)))
     {
-      throw new Kohana_User_Exception('Upgrade Error', Kohana::lang('general_errors.upgrade.failure'));
+        if( false === Kohana::config('core.display_errors'))
+        {
+            // fatal error: the system stops here
+            die( Kohana::lang('setup.error_upgrade_for_end_user') );
+        }
+        else
+        {
+            throw new Kohana_User_Exception(Kohana::lang('general_errors.upgrade.failure'), 'UPGRADE ERROR: ' . $result );
+        }
     }
 
     // if successful, reload the system
