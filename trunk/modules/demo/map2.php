6a3745f82461b0496cc8f0ef79440bbfb80382ab
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-03-02 13:59:00 +0000

    First drop of Reporting code

--- trunk/modules/demo/map2.php
+++ trunk/modules/demo/map2.php
@@ -2,7 +2,7 @@
 <head>
 <?php
 	include '../../client_helpers/map_helper.php';
-	$myMap = new Map('http://localhost:8080/geoserver/', array(0,1,2,3,4,5,6,7,8));
+	$myMap = new Map('http://localhost:8080/geoserver/', array(0,1,2,3,4,5,6,8));
 	$myMap->addIndiciaWFSLayer('Samples', 'topp:states');
 ?>
 <title>Map helper test</title>
---
+++ trunk/modules/indicia_svc_data/libraries/ReportReaders/XMLReportReader.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+* INDICIA
+* @link http://code.google.com/p/indicia/
+* @package Indicia
+*/
+
+/**
+* <h1>XML Report reader</h1>
+* <p>The report reader encapsulates logic for reading reports from a number of sources, and opens up * report methods in a transparent way to the report controller.</p>
+*
+* @package Indicia
+* @subpackage Controller
+* @license http://www.gnu.org/licenses/gpl.html GPL
+* @author Nicholas Clarke <xxx@xxx.net> / $Author$
+* @copyright xxxx
+* @version $Rev$ / $LastChangedDate$
+*/
+
+class XMLReportReader implements ReportReader
+{
+}
