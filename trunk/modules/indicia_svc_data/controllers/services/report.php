cf6e15291cfdb79f7aed15d037bdf3bb2fd76175
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-03-03 15:21:25 +0000

    Removed some things in config examples, modified a few things here and there

--- trunk/modules/indicia_svc_data/controllers/services/report.php
+++ trunk/modules/indicia_svc_data/controllers/services/report.php
@@ -20,26 +20,42 @@
 class Report_Controller extends Service_Base_Controller {
   
   /**
-   * Access the report - probably we will use routing to direct /report directly to /report/access
-   */
+  * Access the report - probably we will use routing to direct /report directly to /report/access
+  * We can specify a request in a number of ways:
+  * <ul>
+  * <li> Predefined report on the core module. </li>
+  * <li> Predefined report elsewhere (URI given). </li>
+  * <li> Report passed with the query. </li>
+  * </ul>
+  * We also need to perform authentication at a read level for the data we're trying to access
+  * (this might be fun, given the low level that the reports run at).
+  * 
+  * A report will have a number of parameters that need to be completed by the requester. Because
+  * this interface is designed to be used by both the core module and the site module, we cannot
+  * directly request this information. As such, we do the following:
+  * <ol>
+  * <li> Grab the report and parse it for parameters. </li>
+  * <li> Cache the report (if it didn't exist on the server already) and assign it a unique id,
+  * which we store temporarily in the Kohana cache. </li>
+  * <li> Send a response back to the requester, inviting them to fill in the parameters. This 
+  * reponse will include the id generated in step 3. </li>
+  * <li> The requester sends back the requested parameters, which are checked against the cache to
+  * ensure they're all there. If not, repeat these steps. </li>
+  * <li> The core retrieves the report from cache, merges the parameters in and executes the query
+  * against the core database. Results are formatted and returned to the requester. </li>
+  * </ol>
+  *
+  * We should also allow submission of parameters with the report, or a combination of this and
+  * requesting them as we go.
+  */
   public function access()
   {
-    /*
-     * We can specify a request in a number of ways:
-     * <ol>
-     * <li> Predefined report on the core module. </li>
-     * <li> Predefined report elsewhere (URI given). </li>
-     * <li> Report passed with the query. </li>
-     * </ol>
-     * We also need to perform authentication at a read level for the data we're trying to access
-     * (this might be fun, given the low level that the reports run at).
-     */
     
   }
   
-  private function fetchLocalRequest($request)
+  private function fetchLocalReport($request)
   {}
   
-  private function fetchRemoteRequest($request)
+  private function fetchRemoteReport($request)
   {}
 }
