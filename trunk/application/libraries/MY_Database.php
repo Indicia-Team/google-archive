edf00aaa030a5ed3c06367207df4fb41d189d1ae
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-03-17 15:40:56 +0000

    Fix as noted in comment to r766

--- trunk/application/libraries/MY_Database.php
+++ trunk/application/libraries/MY_Database.php
@@ -84,7 +84,7 @@ class Database extends Database_Core {
     }
     
     // Wrap geometry fields - and hope this regex works
-    preg_replace('/(?<=[ \t])["|`|\']?(\w+\.\w+_geom)["|`|\']?(?=[ \t])/i', "ST_asText($1)", $sql);
+    preg_replace('/(?<=[ \t])["|`|\']?([\w_]+\.[\w_]+geom)["|`|\']?(?=[ \t])/i', "ST_asText($1)", $sql);
     
     // Fetch the result
     $result = $this->driver->query($this->last_query = $sql);
