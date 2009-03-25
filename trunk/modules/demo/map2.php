9a13e4e0cfdd1146858f7e759f693b0ec36f0464
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-03-25 17:55:49 +0000

    Checking in code before rebasing

--- trunk/modules/demo/map2.php
+++ trunk/modules/demo/map2.php
@@ -2,8 +2,7 @@
 <head>
 <?php
 	include '../../client_helpers/map_helper.php';
-	$myMap = new Map('http://localhost:8080/geoserver/', array(0,1,2,3,4,5,6,8));
-	$myMap->addIndiciaWFSLayer('Samples', 'topp:samples');
+	$myMap = new Map('http://localhost:8080/geoserver/', array(0,1,2,3,4,5,6,8), null, array('input_field_id' => 'entered_sref'));
 ?>
 <title>Map helper test</title>
 </head>
