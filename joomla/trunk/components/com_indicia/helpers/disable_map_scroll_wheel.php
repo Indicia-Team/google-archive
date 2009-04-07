adefb74c85bad86ad1ac23d1fc361e626cede672
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-04-07 15:12:43 +0000

    File deprecated

--- joomla/trunk/components/com_indicia/helpers/disable_map_scroll_wheel.php
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-	jQuery(document).ready(function () {
-		jQuery.each(map.controls, function(i, control) {
-			if (control instanceof OpenLayers.Control.Navigation) {
-				control.disableZoomWheel();
-			}
-		});
-	});
-</script>
