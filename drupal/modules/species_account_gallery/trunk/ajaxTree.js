07671dda9dd112c32ae89ad8995fddfc98b6edb4
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2011-11-30 18:34:48 +0000

    Species account gallery tidy up.

--- drupal/modules/species_account_gallery/trunk/ajaxTree.js
+++ drupal/modules/species_account_gallery/trunk/ajaxTree.js
@@ -1,42 +1,42 @@
+function collapseNode(span) {
+  $(span).parent().find('> .species-gallery-list').slideUp('fast');
+  $(span).removeClass('expanded');
+  $(span).addClass('collapsed');
+}
+
+function expandNode(span) {
+  $(span).parent().find('> .species-gallery-list').slideDown('fast');
+  $(span).removeClass('collapsed');
+  $(span).addClass('expanded');
+}
+
 function loadNode() {
   var tid=this.id.split('-')[1], span=this;
   $.get(
-    'http://localhost/bwars/species_gallery/ajax', 
+    Drupal.settings.galleryAjaxPath, 
     {tid:tid},
     function(data) {
-      $(span).after(data);
+      $(span).parent().append(data);
       $(span).removeClass('not-loaded');
-      $(span).addClass('expanded');
+      expandNode(span);
       // attach image popup plugins to the new content
       if (typeof hs !== "undefined") {
-        $($(span).next().find('a.highslide')).click(function() {
+        $(span).parent().find('> .species-gallery-list a.highslide').click(function() {
           return hs.expand(this);
         });
       }
       if (typeof $.fancybox !== "undefined") {
-        $($(span).next().find('a.fancybox')).fancybox();
+        $(span).parent().find('> .species-gallery-list a.fancybox').fancybox();
       }
     },
     'html'
   );
 }
-
-function collapseNode() {
-  $(this).next().hide();
-  $(this).removeClass('expanded');
-  $(this).addClass('collapsed');
-}
-
-function expandNode() {
-  $(this).next().show();
-  $(this).removeClass('collapsed');
-  $(this).addClass('expanded');
-}
   
 $(document).ready(function() {
   $('ul.species-gallery-list li span.not-loaded').live('click', loadNode);
-  $('ul.species-gallery-list li span.expanded').live('click', collapseNode);
-  $('ul.species-gallery-list li span.collapsed').live('click', expandNode);
+  $('ul.species-gallery-list li span.expanded').live('click', function() { collapseNode(this); });
+  $('ul.species-gallery-list li span.collapsed').live('click', function() { expandNode(this); });
 });
 
 
