4d6c11152cad17a9d56179b6d8877a6198b19b82
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2012-07-02 10:02:17 +0000

    Summary report updates for option for linking to sites rather than spatial queries. Updated CSS to reflect changes for gallery items.

--- drupal/modules/indicia_features/trunk/indicia_features/summary_reports/summary_reports_edit.js
+++ drupal/modules/indicia_features/trunk/indicia_features/summary_reports/summary_reports_edit.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
   
-function enable_map() {
+function enable_site_settings() {
   var enable=$('#edit-field-summary-report-grouping-value-region').attr('checked');
   if (enable) {
     $('#edit-field-summary-report-display-value-map-wrapper').show();
+    $('.group-site-settings').show();
   } else {
     $('#edit-field-summary-report-display-value-map-wrapper').hide();
     // ensure option is not checked as invalid
     $('#edit-field-summary-report-display-value-map').attr('checked', false);
+    $('.group-site-settings').hide();
   }
 }
 
@@ -20,10 +22,10 @@ function enable_map_settings() {
   }
 }
  
-$('#edit-field-summary-report-grouping-value-region').change(enable_map);
-$('#edit-field-summary-report-grouping-value-species-group').change(enable_map);
-$('#edit-field-summary-report-grouping-value-year').change(enable_map);
-$('#edit-field-summary-report-grouping-value-month').change(enable_map);
+$('#edit-field-summary-report-grouping-value-region').change(enable_site_settings);
+$('#edit-field-summary-report-grouping-value-species-group').change(enable_site_settings);
+$('#edit-field-summary-report-grouping-value-year').change(enable_site_settings);
+$('#edit-field-summary-report-grouping-value-month').change(enable_site_settings);
 
 $('#edit-field-summary-report-display-value-map').change(enable_map_settings);
 $('#edit-field-summary-report-display-value-pie').change(enable_map_settings);
@@ -31,7 +33,7 @@ $('#edit-field-summary-report-display-value-bar').change(enable_map_settings);
 $('#edit-field-summary-report-display-value-table').change(enable_map_settings);
 
 // set the initial state
-enable_map();
+enable_site_settings();
 enable_map_settings()
 
 });
