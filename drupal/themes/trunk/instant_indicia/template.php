8d0c69cc429442e327457f62849a4b3d733ec318
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-11-12 21:30:16 +0000

    Removed a debug message

--- drupal/themes/trunk/instant_indicia/template.php
+++ drupal/themes/trunk/instant_indicia/template.php
@@ -5,7 +5,6 @@ function instant_indicia_preprocess_page(&$variables) {
   $pathargs = explode('/',drupal_get_path_alias($_GET['q']));
   if ($pathargs[0] == 'external') {
      // Use template: page__widgets
-     drupal_set_message('iframe');
      $variables['template_file'] = 'page-iframe';
   }
 }
