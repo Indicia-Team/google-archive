82b4f3d4f55aea73717b61492b887622723aa04e
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2013-01-19 21:15:26 +0000

    Update to instant_indicia theme to work with latest Framework theme version.

--- drupal/themes/trunk/instant_indicia/node.tpl.php
+++ drupal/themes/trunk/instant_indicia/node.tpl.php
@@ -1,36 +1,46 @@
-<?php
-// $Id: node.tpl.php,v 1.4 2009/07/13 23:52:58 andregriffin Exp $
-?>
-<div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?>">
+<?php if (!$page): ?>
+  <article id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clearfix">
+<?php endif; ?>
 
-  <?php print $picture ?>
+  <?php if ($picture || $submitted || !$page): ?>
+    <?php if (!$page): ?>
+      <header>
+	<?php endif; ?>
 
-  <?php if ($page == 0): ?>
-    <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-  <?php endif; ?>
+      <?php print $picture ?>
 
-  <?php if ($submitted && !$teaser): ?>
-    <span class="submitted"><?php print $submitted; ?></span>
-  <?php endif; ?>
+	  <?php if (!$page): ?>
+        <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+      <?php endif; ?>
+
+	  <?php if ($submitted && !$teaser): ?>
+        <span class="submitted"><?php print $submitted; ?></span>
+      <?php endif; ?>
+
+    <?php if (!$page): ?>
+      </header>
+	<?php endif; ?>
+  <?php endif;?>
 
   <div class="content">
-    <?php print $content; ?>
+    <?php print $content ?>
   </div>
 
-  <div class="meta clearfix">
+  <?php if (!empty($terms) || !empty($links)): ?>
+    <footer>
+      <?php if ($terms): ?>
+        <div class="terms">
+          <span><?php print t('Tags: ') ?></span><?php print $terms ?>
+        </div>
+      <?php endif;?>
+      <?php if ($links): ?>
+        <div class="links">
+          <?php print $links; ?>
+        </div>
+      <?php endif; ?>
+    </footer>
+  <?php endif;?>
 
-    <?php if ($links): ?>
-      <div class="links">
-        <?php print $links; ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($taxonomy): ?>
-      <div class="terms">
-        <span><?php print t('Tags:') ?></span><?php print $terms ?>
-      </div>
-    <?php endif;?>
-
-  </div>
-
-</div>
+<?php if (!$page): ?>
+  </article> <!-- /.node -->
+<?php endif;?>
