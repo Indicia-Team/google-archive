d6edd241f81f8015243577d76c148b537c43860c
jame2 <jame2@ceh.ac.uk>
2014-03-31 16:30:42 +0000

    Add a theme setting to the mobile app home page.

--- drupal_7/themes/mobile_iform/trunk/mobile_iform/theme-settings.php
+++ drupal_7/themes/mobile_iform/trunk/mobile_iform/theme-settings.php
@@ -7,5 +7,24 @@
  * @param $form_state
  *   A keyed array containing the current state of the form.
  */
-function icpveg_mobile_form_system_theme_settings_alter(&$form, &$form_state)  {
+function mobile_iform_form_system_theme_settings_alter(&$form, &$form_state)  {
+
+  $form['mobile_iform_settings'] = array(
+    '#type' => 'fieldset',
+    '#weight' => -20,
+    '#title' => t('Mobile IForm Configuration'),
+  );
+  $form['mobile_iform_settings']['app_home'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Path to start page.'),
+    '#description' => t(<<<EOD
+Under certain conditions we want to return to the starting point which may not
+be the home page of the website. Enter the path relative to the base path here
+without a leading slash.
+EOD
+          ),
+    '#default_value' => mobile_jquery_theme_get_setting('app_home'),
+    '#weight' => -20,
+  );
+  
 }
