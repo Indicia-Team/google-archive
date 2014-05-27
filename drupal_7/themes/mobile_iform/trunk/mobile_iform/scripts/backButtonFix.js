a5add8fe0e6f9ccfd9051b0012ea04a61388d0d0
jame2 <jame2@ceh.ac.uk>
2014-05-27 16:53:39 +0000

    Fixes to get back buttons working and change to stop form being opened in browser when running in standalone mode.

--- drupal_7/themes/mobile_iform/trunk/mobile_iform/scripts/backButtonFix.js
+++ drupal_7/themes/mobile_iform/trunk/mobile_iform/scripts/backButtonFix.js
@@ -14,23 +14,24 @@
 */
 
 var BACK_BUTTON_URLS = {
-  'tab-location'  :  'home',
+  'app-home' : '/record/app',
+  'tab-location'  :  '/record/app/home',
   'tab-species'  :  '#tab-location',
   'tab-photograph'  :  '#tab-species',
   'tab-injury'  :  '#tab-photograph',
   'tab-weather'  :  '#tab-injury',
   'tab-pollution'  :  '#tab-weather',
-  'app-examples-.*' : 'examples',
-  'app-symptoms-.*' : 'symptoms',
-  'app-other-causes-.*' : 'other-causes',
-  'app-.*' :  'home'
+  'app-examples-.*' : '/record/app/examples',
+  'app-symptoms-.*' : '/record/app/symptoms',
+  'app-other-causes-.*' : '/record/app/other-causes',
+  'app-.*' :  '/record/app/home'
 };
 
 /*
  * Fixes back buttons for specific page
  */
 function fixPageBackButtons(page_id){
-  console.log('FIXING: back buttons ( ' + page_id + ')');
+  console.log('FIXING: back buttons (' + page_id + ')');
 
   var url = "";
   //check if in array
@@ -53,26 +54,74 @@ function fixPageBackButtons(page_id){
     jQuery(button).attr('href', url);   
     //remove data-rel="back" attribute
     jQuery(button).removeAttr('data-rel');
+    //use reverse transition for back button
+    jQuery(button).attr('data-direction', 'reverse');
   });
 }
+/************ END BACK BUTTON FIX ****************************************************/
+
+/************** STANDALONE LINK FIX **************************************************/
+function fixForwardButtons() {
+  var links = jQuery("a[data-ajax='false']");
+  links.each(function(index, link) {
+    var $link = jQuery(link);
+    console.log('FIXING: link ( ' + $link.attr('href') + ')');
+    $link.on('click', function(event){
+      event.preventDefault();
+      location.href = jQuery(event.target).attr('href');
+    });
+  });
+  
+}
+/************ END STANDALONE LINK FIX ************************************************/
+
+
+
+/* Fix buttons on pagecreate event */
 
 jQuery(document).on('pagecreate', function(event, ui) {
      if (browserDetect('Safari')){
        fixPageBackButtons(event.target.id);
+       if(("standalone" in window.navigator) && window.navigator.standalone) {
+         // We are in iOS standalone mode.
+         // Links that have the data-prefetch attribute will load standalone but
+         // links that have data-ajax = "false" will open in the browser
+         // Lets fix this too.
+         fixForwardButtons();
+     }
+         
      }
 });
-/************ END BACK BUTTON FIX ****************************************************/
 
 /*
  * Generic function to detect the browser
  * 
- * Chrome has to have and ID of both Chrome and Safari therefore
- * Safari has to have an ID of only Safari and not Chrome
+ * Chrome has to have and ID of both Chrome and AppleWebKit while
+ * Safari has to have an ID of only AppleWebKit and not Chrome
+ * 
+ * Examples of User Agent
+ * 
+ * In iOS/Safari:
+ * Mozilla/5.0 (iPad; CPU OS 7_1_1 like Mac OS X) AppleWebKit/537.51.2 
+ * (KHTML, like Gecko) Version/7.0 Mobile/11D201 Safari/9537.53
+ * 
+ * In iOS/standalone
+ * Mozilla/5.0 (iPad; CPU OS 7_1_1 like Mac OS X) AppleWebKit/537.51.2 
+ * (KHTML, like Gecko) Version/7.0 Mobile/11D201
+ * 
+ * In Windows/Chrome
+ * Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) 
+ * Chrome/35.0.1916.114 Safari/537.36
+ * 
+ * In Android/Chrome
+ * Mozilla/5.0 (Linux; Android 4.4.2; Nexus 5 Build/KOT49H) AppleWebKit/537.36 
+ * (KHTML, like Gecko) Chrome/30.0.1599.105 Mobile safari/537.36
+ * 
  */
 function browserDetect(browser){
     if (browser == 'Chrome' || browser == 'Safari'){
         var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
-        var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+        var is_safari = navigator.userAgent.indexOf("AppleWebKit") > -1;
 
         if (is_safari){
           if (browser == 'Chrome'){
