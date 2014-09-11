1dd2f0a994000f3537c9b524126a81c9fbf14c6b
karolis.kazlauskis <karolis.kazlauskis@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-09-11 10:19:02 +0000

    Fixed object copying function and improved geolocation error feedback

--- drupal_7/modules/iform_mobile/trunk/iform_mobile/js/helper.js
+++ drupal_7/modules/iform_mobile/trunk/iform_mobile/js/helper.js
@@ -58,7 +58,7 @@ function objClone(obj) {
     if (null == obj || "object" != typeof obj) return obj;
     var copy = obj.constructor();
     for (var attr in obj) {
-        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+        if (obj.hasOwnProperty(attr)) copy[attr] = objClone(obj[attr]);
     }
     return copy;
 }
