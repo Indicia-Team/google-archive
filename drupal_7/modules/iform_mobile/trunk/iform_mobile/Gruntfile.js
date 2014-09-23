49d2975f845c51b1177595f662f2304f44b40df0
karolis.kazlauskis <karolis.kazlauskis@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-09-23 07:31:01 +0000

    Added Grunt support.

---
+++ drupal_7/modules/iform_mobile/trunk/iform_mobile/Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        karma: {
+            unit: {
+                configFile: 'test/karma.conf.js'
+            }
+        },
+        concat: {
+            options: {
+                // define a string to put between each file in the concatenated output
+                separator: '\n\n'
+            },
+            dist: {
+                // the files to concatenate
+                src: ['src/*.js'],
+                // the location of the resulting JS file
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                // the banner is inserted at the top of the output
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-karma');
+
+    // the default task can be run just by typing "grunt" on the command line
+    grunt.registerTask('default', ['karma', 'concat', 'uglify']);
+
+};
