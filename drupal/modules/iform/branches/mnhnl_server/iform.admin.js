ff5d694d4e4729d6f0024a69220fdad807ecddf4
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-11-10 10:26:11 +0000

    Removed unused branch

--- drupal/modules/iform/branches/mnhnl_server/iform.admin.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('input[name="indicia_warehouse"]').change(function(evt) {
-    if (evt.target.value=='other') {
-      $('#warehouse_details').removeClass('collapsed');
-    } else {
-      $('#warehouse_details').addClass('collapsed');
-    }
-  });
-});
