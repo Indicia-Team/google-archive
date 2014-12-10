348428e81ca3df7bf9df3b88ac42819fd2f81613
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-12-10 20:58:33 +0000

    Icons in explore grids - show both certainty and record status.

--- drupal/themes/trunk/instant_indicia/template.php
+++ drupal/themes/trunk/instant_indicia/template.php
@@ -3,8 +3,14 @@
 function instant_indicia_preprocess_page(&$variables) {
   // Check path to determine widget pages
   $pathargs = explode('/',drupal_get_path_alias($_GET['q']));
-  if ($pathargs[0] == 'external') {
+  if ($pathargs[0] == 'external' || (!empty($_GET['external']) && $_GET['external']==='t')) {
      // Use template: page__widgets
      $variables['template_file'] = 'page-iframe';
+  } elseif ($pathargs[0] == 'popup' || (!empty($_GET['popup']) && $_GET['popup']==='t')) {
+     // Use template: page__widgets
+     $variables['template_file'] = 'page-popup';
   }
+  
+  drupal_set_html_head('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">');
+  $variables['head'] = drupal_get_html_head();
 }
